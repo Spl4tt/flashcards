349705c1cd04e8aa0c7b659cccb0af53b77c21b9
Ky <kyrill@gobber.ch>
2021-05-24 15:11:14 +0200

    More middleware stuff learned

--- app.js
+++ app.js
@@ -10,6 +10,23 @@ app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
+app.use((req, res, next) => {
+    // req.message = 'This message made it, juhu';
+    console.log('Hello');
+    // const err = new Error('Error happened');
+    // err.status = 500;
+    next();
+});
+
+app.use((req, res, next) => {
+    // console.log(req.message);
+    console.log('world');
+    next();
+});
+
+
+
+
 app.get('/', (req, res) => {
     const name = req.cookies.username
     if(name) {
@@ -41,6 +58,19 @@ app.post('/hello', (req, res) => {
 app.post('/goodbye', (req, res) => {
     res.clearCookie('username');
     res.redirect('/hello');
+});
+
+app.use((req, res, next) => {
+    const err = new Error('Net gfunde, sry');
+    err.status = 404;
+    next(err);
+});
+
+//Error handler
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
 });
 
 app.listen(3000, () => {
